1e0124041f1d71248a7bac123d64deb863aa3e4e
Andy Dent <me@andydent.com>
2024-10-06 11:40:50 +0800

    MateriallyMessy - Adding first standard material icon on screen MainActivity.kt - imports added from androidx.compose   foundation.layout.Column   material.icons.Icons   material.icons.rounded.ShoppingCart   ui.Alignment   ui.res.stringResource - Greeting   - add a Column wrapping the Text   - add an Icon below the Text

--- MateriallyMessy/MateriallyMessy Code Change Diary.md
+++ MateriallyMessy/MateriallyMessy Code Change Diary.md
@@ -10,3 +10,22 @@ Project created following [Android docs][a1] using _Empty Activity_ template
 - Min SDK API 28
 
 [a1]: https://developer.android.com/develop/ui/compose/setup
+
+
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+## Adding first standard material icon on screen
+2024-10-05
+
+MainActivity.kt
+- imports added from androidx.compose
+  foundation.layout.Column
+  material.icons.Icons
+  material.icons.rounded.ShoppingCart
+  ui.Alignment
+  ui.res.stringResource
+- Greeting 
+  - add a Column wrapping the Text
+  - add an Icon below the Text
+
+
+[m1]: https://developer.android.com/reference/kotlin/androidx/compose/material/icons/package-summary
--- MateriallyMessy/app/src/main/java/com/aussie/materiallymessy/MainActivity.kt
+++ MateriallyMessy/app/src/main/java/com/aussie/materiallymessy/MainActivity.kt
@@ -6,10 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ShoppingCart
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+
 import androidx.compose.ui.tooling.preview.Preview
 import com.aussie.materiallymessy.ui.theme.MateriallyMessyTheme
 
@@ -32,10 +39,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Greetings $name!",
-        modifier = modifier
-    )
+    Column {
+
+        Text(
+            text = "Greetings $name",
+            modifier = modifier
+        )
+        Icon(
+            Icons.Rounded.ShoppingCart,
+            contentDescription = "just a test",
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+    }
 }
 
 @Preview(showBackground = true)
