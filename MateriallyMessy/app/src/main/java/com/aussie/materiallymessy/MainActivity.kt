90543b65b995d63c52ad99c6434b606399260260
Andy Dent <me@andydent.com>
2024-10-06 18:19:14 +0800

    MateriallyMessyh - minor imports cleanup by Android Studio

--- MateriallyMessy/app/src/main/java/com/aussie/materiallymessy/MainActivity.kt
+++ MateriallyMessy/app/src/main/java/com/aussie/materiallymessy/MainActivity.kt
@@ -4,19 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.Column
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ShoppingCart
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-
 import androidx.compose.ui.tooling.preview.Preview
 import com.aussie.materiallymessy.ui.theme.MateriallyMessyTheme
 
